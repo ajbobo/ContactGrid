2026b60c87730d960cf25660a5111c310cb4112d
AJB <none@none>
2011-10-13 15:30:09 +0000

    Removed an unused import

--- src/ajbobo/contactgrid/ContactGrid.java
+++ src/ajbobo/contactgrid/ContactGrid.java
@@ -12,7 +12,6 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.provider.Contacts;
 import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.AdapterView;
